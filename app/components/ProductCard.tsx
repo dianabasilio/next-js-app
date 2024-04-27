c2aa7b09ab158b5f1c152687a590effb1c0ab46c
DIANA KAREN BASILIO BELTRAN <dbasilio@100-dbasilio.local>
2024-04-26 22:15:15 -0600

    Server components can no use interactactions THIS CAUSES an error.

---
+++ app/components/ProductCard.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const ProductCard = () => {
+  return (
+    <div>
+      <button
+        onClick={() => {
+          console.log("CLICK");
+        }}
+      >
+        Add to cart
+      </button>
+    </div>
+  );
+};
+
+export default ProductCard;
