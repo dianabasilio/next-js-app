5238690344e15477cdc15fc01aac6c84dfbcdfd1
DIANA KAREN BASILIO BELTRAN <dbasilio@100-dbasilio.local>
2024-05-03 13:21:54 -0600

    Dynamic rendering.

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -8,7 +8,7 @@ interface User {
 const UsersPage = async () => {
   async function getUsers(): Promise<User[]> {
     const response = await fetch("https://jsonplaceholder.typicode.com/users", {
-      next: { revalidate: 10 },
+      cache: "no-store",
     });
     const users = await response.json();
     return users; // this should be an array of User objects
@@ -19,6 +19,7 @@ const UsersPage = async () => {
   return (
     <>
       <h1>Users</h1>
+      <p>{new Date().toLocaleTimeString()}</p>
       <ul>
         {users.map((user) => (
           <li key={user.id}>{user.name}</li>
